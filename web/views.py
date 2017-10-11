e0ab40a01b756bfc3b5be2f55a9065bc10661e3d
shsh0889 <seosh0889@gmail.com>
2017-10-11 16:05:14 +0900

    Bootstrap Test

--- web/views.py
+++ web/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     me = User.objects.get(username='root')
     posts = Post.objects.filter(author=me)
     return render(request, 'web/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'web/post_detail.html', {'post': post})
